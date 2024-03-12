977c0909e65c4700d5847297650eac184aaacc4a
Laura Molina <ld.molina11@uniandes.edu.co>
2024-03-12 18:16:59 -0500

    :sparkles: calendar

--- app/src/main/java/com/misoux/departtime/CreateAlarmActivity.kt
+++ app/src/main/java/com/misoux/departtime/CreateAlarmActivity.kt
@@ -6,8 +6,14 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.CheckBox
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputLayout
+import androidx.core.util.Pair
+
 
 class CreateAlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +31,39 @@ class CreateAlarmActivity : AppCompatActivity() {
         val items = listOf("5 minutos", "10 minutos", "15 minutos", "30 minutos", "1 hora")
         val adapter = ArrayAdapter(this, R.layout.dropdown_menu_popup_item, items)
         val dropdownMenu = findViewById<AutoCompleteTextView>(R.id.dropdown_autocomplete)
+        val dropdownMenuInput = findViewById<TextInputLayout>(R.id.dropdown_menu)
         val checkBox = findViewById<CheckBox>(R.id.check_in_advance)
-
         dropdownMenu.setAdapter(adapter)
 
         dropdownMenu.setText("5 minutos", false)
 
-        dropdownMenu.isEnabled = checkBox.isChecked
-
         checkBox.setOnCheckedChangeListener { _, isChecked ->
-            dropdownMenu.isEnabled = isChecked
-
+            dropdownMenuInput.isEnabled = isChecked
         }
 
         val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.dropdown_autocomplete)
 
         val dropDownBackground = ColorDrawable(ContextCompat.getColor(this, R.color.surface))
         autoCompleteTextView.setDropDownBackgroundDrawable(dropDownBackground)
+
+
+        val buttonCalendar: MaterialButton = findViewById(R.id.button_calendar)
+
+        val constraintsBuilder = CalendarConstraints.Builder()
+            .setOpenAt(MaterialDatePicker.todayInUtcMilliseconds())
+            .setValidator(DateValidatorPointForward.now())
+
+        buttonCalendar.setOnClickListener {
+            val datePickerDialog = MaterialDatePicker.Builder.datePicker()
+                .setTheme(R.style.CustomMaterialDatePickerStyle)
+                .setTitleText("Selecciona una fecha o rango de fechas")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setCalendarConstraints(constraintsBuilder.build())
+                .build()
+
+
+            datePickerDialog.show(supportFragmentManager, "datePicker")
+        }
 
     }
 }
